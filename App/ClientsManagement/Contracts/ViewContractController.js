d97be5c960d48f9e712b1823e7d954938229bb2c
INNROAD\sujithkumar.pallapat <sujithkumar.pallapati@innroad.com>
2017-05-05 22:13:41 +0530

    View COntract Changes

--- App/ClientsManagement/Contracts/ViewContractController.js
+++ App/ClientsManagement/Contracts/ViewContractController.js
@@ -1,4 +1,23 @@
 ﻿securityApp.controller('ViewContractController', function ($scope, $state, $stateParams, $location, ContractManagementService, ngNotify, flowFactory) {
 
+    $scope.ContractId = $stateParams.ContractId;
+    $scope.contractInfo = {};
 
+
+    ContractManagementService.getContractInformation($scope.ContractId).then(function (result) {
+        if (result) {
+            console.log(result);
+
+            $scope.contractInfo = result;
+        } else {
+            ngNotify.set('Unable to get client details',
+                    {
+                        theme: 'pure',
+                        position: 'top',
+                        type: 'error',
+                        button: 'true',
+                        sticky: 'false',
+                    });
+        }
+    });
 });
